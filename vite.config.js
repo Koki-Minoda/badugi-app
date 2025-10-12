8c7ccd2a5eb742f55db26f6795b940350780ada5
Koki-Minoda <fireflower.hirona.luv@gmail.com>
2025-10-12 14:31:18 +0900

    chore: add vite path aliases

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// --- Path Aliases Added ---
+import path from 'path';
+export default {
+  resolve: {
+    alias: {
+      '@core': path.resolve(__dirname, 'src/core'),
+      '@games': path.resolve(__dirname, 'src/games'),
+      '@ui': path.resolve(__dirname, 'src/ui'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@audio': path.resolve(__dirname, 'src/audio'),
+    },
+  },
+};

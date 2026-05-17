bda292537c1943a56a0843b556c9634ac8c0ba21
Koki-Minoda <fireflower.hirona.luv@gmail.com>
2026-05-18 01:14:53 +0900

    test(browser): isolate core five matrix step B divergence

--- src/ui/qa/exposeBrowserGameplayState.js
+++ src/ui/qa/exposeBrowserGameplayState.js
@@ -1,13 +1,15 @@
 import { installBrowserGameplayTraceGlobals } from "./browserGameplayTrace.js";
 import { collectBrowserGameplaySnapshot } from "./collectBrowserGameplaySnapshot.js";
+import { appendSnapshotMergeSourceTrace } from "./traceSnapshotMergeSource.js";
 
 export function exposeBrowserGameplayState() {
   if (typeof window === "undefined") return null;
   installBrowserGameplayTraceGlobals();
   window.__MGX_GET_GAMEPLAY_SNAPSHOT__ = collectBrowserGameplaySnapshot;
+  window.__MGX_TRACE_SNAPSHOT_MERGE_SOURCE__ = appendSnapshotMergeSourceTrace;
   return {
     getSnapshot: window.__MGX_GET_GAMEPLAY_SNAPSHOT__,
+    getSnapshotMergeSource: window.__MGX_TRACE_SNAPSHOT_MERGE_SOURCE__,
     clearTrace: window.__MGX_CLEAR_GAMEPLAY_TRACE__,
   };
 }
-
